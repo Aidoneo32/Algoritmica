bf89f4fd0bdb2b5e980bb193bc7841c38230a8c7
Aidoneo32 <24020072@uadeo.mx>
2024-09-08 19:32:23 -0700

    Hola Mundo2

--- main.cpp
+++ main.cpp
@@ -5,5 +5,10 @@
 int main()
 {
     printf("hola mundo");
+    printf("/hola mundo");      //hola//
+    printf("/nX     /nY");  //mundo//
+    printf("/n      /n    hola mundo");    //david//
+    printf("/n      /n    /n       hola mundo");
+
 
 }
