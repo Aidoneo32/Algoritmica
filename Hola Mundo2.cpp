7d40694c8b170a9a287b7e2787169a26a0fc4760
Aidoneo32 <24020072@uadeo.mx>
2024-09-22 21:48:08 -0700

    Add files via upload

---
+++ Hola Mundo2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+
+int main()
+{
+    printf("hola mundo");
+    printf("/hola mundo");      //hola//
+    printf("/nX     /nY");  //mundo//
+    printf("/n      /n    hola mundo");    //david//
+    printf("/n      /n    /n       hola mundo");
+
+
+}
