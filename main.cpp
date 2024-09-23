110316f187e5ae38dd022300ada7ca76af0e2918
Aidoneo32 <24020072@uadeo.mx>
2024-09-22 21:29:03 -0700

    Conversion De Gragos C++

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
-#include <stdio.h>
+#include <iostream>//usar
 
-
+using namespace std;//usamos espacio de nombre estandar cout (salida), cin(entrada), cerr (salida de errores),
+//clog (se usa para mensaje de log
 
 int main()
 {
-    printf("hola mundo");
-    printf("/hola mundo");      //hola//
-    printf("/nX     /nY");  //mundo//
-    printf("/n      /n    hola mundo");    //david//
-    printf("/n      /n    /n       hola mundo");
+    float celsius, fahrenheit;
 
+    cout <<"Grados Celsius: ";
+    cin >> celsius;
 
+    fahrenheit = (celsius* 9 / 5) +32;
+
+    cout <<celsius << "°C son" << fahrenheit << "°F" << endl;
+
+    return 0;
 }
